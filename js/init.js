9dffbcc8ff35e1fa637fa67181ba344e85b21f8e
edugw <newitweb@gmail.com>
2022-05-09 07:59:27 -0300

    Inicializa productos solo una vez

--- js/init.js
+++ js/init.js
@@ -1,32 +1,33 @@
     
 // Carga Productos y carrito en Localstorage
 function initSite(){
-    const productosSitio= [];
-    const ravioles= new articulo ("Ravioles", "Ravioles Plancha de 450 gramos",550, 450, "images/raviolesdet.webp", 
-        true
-    );
+    if (localStorage.getItem('datos') == null){
+        const productosSitio= [];
+        const ravioles= new articulo ("Ravioles", "Ravioles Plancha de 450 gramos",550, 450, "images/raviolesdet.webp", 
+            true
+        );
 
-    productosSitio.push(ravioles);
+        productosSitio.push(ravioles);
 
-    const tallarines= new articulo ("Tallarines", "Tallarines elaborados en el día",430, 430, "images/fideosdet.webp", 
-        true
-    );
+        const tallarines= new articulo ("Tallarines", "Tallarines elaborados en el día",430, 430, "images/fideosdet.webp", 
+            true
+        );
 
-    productosSitio.push(tallarines);
+        productosSitio.push(tallarines);
 
-    const tuco= new articulo ("Tuco", "Tuco fileto 400 gramos",250, 250, "images/tucodet.webp", 
-        true
-    );
-    productosSitio.push (tuco);
+        const tuco= new articulo ("Tuco", "Tuco fileto 400 gramos",250, 250, "images/tucodet.webp", 
+            true
+        );
+        productosSitio.push (tuco);
 
-    const pizza= new articulo ("Pizza", "Prepizza elaborada con la mejor harina",380, 380, "images/pizzadet.webp", 
-        true
-    );
-    productosSitio.push (pizza);
+        const pizza= new articulo ("Pizza", "Prepizza elaborada con la mejor harina",380, 380, "images/pizzadet.webp", 
+            true
+        );
+        productosSitio.push (pizza);
 
-    // Guardo la lista de productos como un string en localstorage
-    localStorage.setItem('datos', JSON.stringify(productosSitio));
-
+        // Guardo la lista de productos como un string en localstorage
+        localStorage.setItem('datos', JSON.stringify(productosSitio));
+    }
       // Inicializa por primera vez el carrito de productos comprados
     if (localStorage.getItem('carrito') == null){
       let vArticulos= [];
